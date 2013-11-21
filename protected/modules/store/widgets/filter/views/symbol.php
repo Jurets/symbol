96c302a9877d4889e6c84755b8b8d23046c7a717
fedman <fedmanbfg@mail.ru>
2013-11-21 12:31:42 +0200

    добавил в фильтры поле -не выбрано-

--- protected/modules/store/widgets/filter/views/symbol.php
+++ protected/modules/store/widgets/filter/views/symbol.php
@@ -5,13 +5,17 @@
 foreach($attributes as $attrData)
 {
     $limits = array();
+    $arr = array();
+    
     foreach($attrData['filters'] as $filter)
     {
         $url = Yii::app()->request->addUrlParam('/store/category/view', array($filter['queryKey'] => $filter['queryParam']), $attrData['selectMany']);
         $limits[$url] = $filter['title'];            
     }
+    $arr[Yii::app()->request->removeUrlParam('/store/category/view', $filter['queryKey'])] = '-не выбран-';
+    $limits = $arr + $limits;
     echo '<div>';
-    echo '<label for="">'.mb_substr($attrData["title"], 0,5,'UTF-8').'</label>';
+    echo '<label for="">'.mb_substr($attrData["title"], 0,8,'UTF-8').'</label>';
     echo CHtml::dropDownList($attrData['title'], Yii::app()->request->url, $limits, array('onchange'=>'applyCategorySorter(this)'));
     echo '</div>';
 
