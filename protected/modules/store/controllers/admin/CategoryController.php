56ab5ec316ff01186493715fbef4242e3cff3616
Jurets <jurets75@gmail.com>
2013-11-22 20:45:39 +0200

    исправление имени класса (в верблюд)

--- protected/modules/store/controllers/admin/CategoryController.php
+++ protected/modules/store/controllers/admin/CategoryController.php
@@ -5,129 +5,130 @@
  */
 class CategoryController extends SAdminController {
 
-    public function filters() {
-        return array(
-            'rights',
-            'ajaxOnly + moveNode',
-        );
-    }
+	public function filters() {
+		return array(
+			'rights',
+			'ajaxOnly + moveNode',
+		);
+	}
 
-    public function actionIndex()
-    {
-        $this->actionUpdate(true);
-    }
+	public function actionIndex()
+	{
+		$this->actionUpdate(true);
+	}
 
-    public function actionCreate()
-    {
-        $this->actionUpdate(true);
-    }
+	public function actionCreate()
+	{
+		$this->actionUpdate(true);
+	}
 
-    public function actionUpdate($new = false)
-    {
-        if ($new === true)
-            $model = new StoreCategory;
-        else
-        {
-            $model = StoreCategory::model()
-                ->language($_GET)
-                ->findByPk($_GET['id']);
-        }
+	public function actionUpdate($new = false)
+	{
+		if ($new === true)
+			$model = new StoreCategory;
+		else
+		{
+			$model = StoreCategory::model()
+				->language($_GET)
+				->findByPk($_GET['id']);
+		}
 
-        if (!$model)
-            throw new CHttpException(404, Yii::t('StoreModule.admin', 'Категория не найдена.'));
+		if (!$model)
+			throw new CHttpException(404, Yii::t('StoreModule.admin', 'Категория не найдена.'));
 
-        $form = new STabbedForm('application.modules.store.views.admin.category.categoryForm', $model);
-        $form->formWidget = 'zii.widgets.jui.CJuiTabs';
-        $form->summaryOnEachTab = true;
-        
+		$form = new STabbedForm('application.modules.store.views.admin.category.categoryForm', $model);
+		$form->formWidget = 'zii.widgets.jui.CJuiTabs';
+		$form->summaryOnEachTab = true;
+
         $cr = new CDbCriteria;
         $cr->addNotInCondition('StoreAttribute.id', CHtml::listData($model->categoryAttributeRelation, 'attribute_id','attribute_id'));
         $allAttributes = StoreAttribute::model()->findAll($cr);
         $filterTab = $this->renderPartial('_options', array('model'=>$model,'attributes'=>$allAttributes), true);
         $form->content = array(Yii::t('main', 'Фильтры')=> $filterTab);
 //        $form->tabs = array('filter'=> $x);
+        
+        
+		if (Yii::app()->request->isPostRequest)
+		{
+			$model->attributes = $_POST['StoreCategory'];
 
-        if (Yii::app()->request->isPostRequest)
-        {
-            $model->attributes = $_POST['StoreCategory'];
-
-            if ($model->validate())
-            {
-                $parent=StoreCategory::model()->findByPk(1);
-                if($model->getIsNewRecord())
-                    $model->appendTo($parent);
-                else
-                    $model->saveNode();
+			if ($model->validate())
+			{
+				$parent=StoreCategory::model()->findByPk(1);
+				if($model->getIsNewRecord())
+					$model->appendTo($parent);
+				else
+					$model->saveNode();
                 // Set type attributes
                 $model->useAttributes(Yii::app()->request->getPost('attributes', array()));
-                
-                $this->setFlashMessage(Yii::t('StoreModule.admin', 'Изменения успешно сохранены'));
 
-                if (isset($_POST['REDIRECT']))
-                    $this->smartRedirect($model);
-                else
-                    $this->redirect(array('create'));
-            }
-        }
+				$this->setFlashMessage(Yii::t('StoreModule.admin', 'Изменения успешно сохранены'));
 
-        $this->render('update', array(
-            'model'=>$model,
-            'form'=>$form,
-        ));
-    }
+				if (isset($_POST['REDIRECT']))
+					$this->smartRedirect($model);
+				else
+					$this->redirect('create');
+			}
+		}
 
-    /**
-     * Drag-n-drop nodes
-     */
-    public function actionMoveNode()
-    {
-        $node = StoreCategory::model()->findByPk($_GET['id']);
-        $target = StoreCategory::model()->findByPk($_GET['ref']);
+		$this->render('update', array(
+			'model'=>$model,
+			'form'=>$form,
+		));
+	}
 
-        if((int) $_GET['position'] > 0)
-        {
-            $pos = (int) $_GET['position'];
-            $childs = $target->children()->findAll();
-            if(isset($childs[$pos-1]) && $childs[$pos-1] instanceof StoreCategory && $childs[$pos-1]['id'] != $node->id)
-                $node->moveAfter($childs[$pos-1]);
-        }
-        else
-            $node->moveAsFirst($target);
-    }
+	/**
+	 * Drag-n-drop nodes
+	 */
+	public function actionMoveNode()
+	{
+		$node = StoreCategory::model()->findByPk($_GET['id']);
+		$target = StoreCategory::model()->findByPk($_GET['ref']);
 
-    /**
-     * Redirect to category front.
-     * @param $id
-     */
-    public function actionRedirect($id)
-    {
-        $node = StoreCategory::model()->findByPk($_GET['id']);
-        $this->redirect($this->createUrl('/store/category/view', array('url'=>$node->url)));
-    }
+		if((int) $_GET['position'] > 0)
+		{
+			$pos = (int) $_GET['position'];
+			$childs = $target->children()->findAll();
+			if(isset($childs[$pos-1]) && $childs[$pos-1] instanceof StoreCategory && $childs[$pos-1]['id'] != $node->id)
+				$node->moveAfter($childs[$pos-1]);
+		}
+		else
+			$node->moveAsFirst($target);
+	}
 
-    /**
-     * Delete category
-     * @param array $id
-     */
-    public function actionDelete($id)
-    {
-        if (Yii::app()->request->isPostRequest)
-        {
-            $model = StoreCategory::model()->findByPk($id);
+	/**
+	 * Redirect to category front.
+	 * @param $id
+	 */
+	public function actionRedirect($id)
+	{
+		$node = StoreCategory::model()->findByPk($_GET['id']);
+		$this->redirect($this->createUrl('/store/category/view', array('url'=>$node->url)));
+	}
 
-            if($model->descendants()->count() > 0)
-                throw new CHttpException(424, Yii::t('StoreModule.admin','Ошибка удаления категории. Сперва нужно удалить все подкатегории.'));
+	/**
+	 * Delete category
+	 * @param array $id
+	 */
+	public function actionDelete($id)
+	{
+		if (Yii::app()->request->isPostRequest)
+		{
+			$model = StoreCategory::model()->findByPk($id);
 
-            if(StoreProductCategoryRef::model()->countByAttributes(array('category'=>$model->id, 'is_main'=>'1')) > 0)
-                throw new CHttpException(424, Yii::t('StoreModule.admin','Ошибка удаления категории. Сперва нужно удалить все товары из категории.'));
+			if($model->descendants()->count() > 0)
+				throw new CHttpException(424, Yii::t('StoreModule.admin','Ошибка удаления категории. Сперва нужно удалить все подкатегории.'));
 
-            //Delete if not root node
-            if ($model && $model->id != 1)
-                $model->deleteNode();
+			if(StoreProductCategoryRef::model()->countByAttributes(array('category'=>$model->id, 'is_main'=>'1')) > 0)
+				throw new CHttpException(424, Yii::t('StoreModule.admin','Ошибка удаления категории. Сперва нужно удалить все товары из категории.'));
 
-            if (!Yii::app()->request->isAjaxRequest)
-                $this->redirect('create');
-        }
-    }
+			//Delete if not root node
+			if ($model && $model->id != 1)
+				$model->deleteNode();
+
+			if (!Yii::app()->request->isAjaxRequest)
+				$this->redirect('create');
+		}
+	}
 
 }
